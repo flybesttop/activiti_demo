d4d3e6039a0f7666c11b0feb7f0742d05aa7d523
flybesttop <1602803469@qq.com>
2023-01-03 00:11:39 +0800

    init

--- src/test/java/com/mtx/activiti/ActivitiDemo.java
+++ src/test/java/com/mtx/activiti/ActivitiDemo.java
@@ -2,11 +2,15 @@ package com.mtx.activiti;
 
 import org.activiti.engine.*;
 import org.activiti.engine.repository.Deployment;
+import org.activiti.engine.repository.ProcessDefinition;
+import org.activiti.engine.repository.ProcessDefinitionQuery;
 import org.activiti.engine.runtime.ProcessInstance;
 import org.activiti.engine.task.Task;
 import org.junit.jupiter.api.Test;
 
+import java.io.InputStream;
 import java.util.List;
+import java.util.zip.ZipInputStream;
 
 public class ActivitiDemo {
 
@@ -59,7 +63,7 @@ public class ActivitiDemo {
 
         List<Task> taskList = taskService.createTaskQuery()
                 .processDefinitionKey("evection")
-                .taskAssignee("张三")
+                .taskAssignee("赵六")
                 .list();
 
         for (Task task : taskList) {
@@ -67,6 +71,9 @@ public class ActivitiDemo {
             System.out.println("任务id：" + task.getId());
             System.out.println("任务负责人：" + task.getAssignee());
             System.out.println("任务名称：" + task.getName());
+
+            // 获取任务之后并且完成任务
+            taskService.complete(task.getId());
         }
 
     }
@@ -75,11 +82,65 @@ public class ActivitiDemo {
      * 完成任务
      */
     @Test
-    public void completeTask(){
+    public void completeTask() {
 
         ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
         TaskService taskService = processEngine.getTaskService();
         taskService.complete("2505");
+
+    }
+
+    @Test
+    public void deployProcessByZip() {
+        ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+
+        RepositoryService repositoryService = processEngine.getRepositoryService();
+
+        InputStream is = this.getClass()
+                .getClassLoader()
+                .getResourceAsStream("bpmn/evection.zip");
+
+        ZipInputStream zipInputStream = new ZipInputStream(is);
+        // 使用压缩包进行流程部署
+        Deployment deployment = repositoryService.createDeployment()
+                .addZipInputStream(zipInputStream)
+                .deploy();
+    }
+
+    /**
+     * 查询流程定义
+     * act_re_procdef
+     */
+    @Test
+    public void queryProcessDefinition() {
+        ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+        RepositoryService repositoryService = processEngine.getRepositoryService();
+
+        ProcessDefinitionQuery processDefinitionQuery = repositoryService.createProcessDefinitionQuery();
+
+        // 返回流程定义信息集合
+        List<ProcessDefinition> evectionList = processDefinitionQuery.processDefinitionKey("evection")
+                .orderByProcessDefinitionVersion()
+                .desc()
+                .list();
+
+        for (ProcessDefinition processDefinition : evectionList) {
+            System.out.println("流程定义id：" + processDefinition.getId());
+            System.out.println("流程定义key：" + processDefinition.getKey());
+            System.out.println("流程定义名称：" + processDefinition.getName());
+            System.out.println("流程定义版本：" + processDefinition.getVersion());
+            System.out.println("流程部署id："+ processDefinition.getDeploymentId());
+        }
+
+    }
+
+    @Test
+    public void deleteDeploy() {
+        ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+
+        RepositoryService repositoryService = processEngine.getRepositoryService();
+
+        repositoryService.deleteDeployment("1");
 
     }
 
